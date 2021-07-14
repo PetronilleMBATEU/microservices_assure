6c94bf80d0bc63688097f5d4a8d838703eafb7b7
Petro M <raissa.mbateu@gmail.com>
2021-07-14 21:08:10 +0200

    first commit

---
+++ src/main/java/com/urbanisation_si/microservices_assure/dao/AssureRepository.java
@@ -0,0 +1,9 @@
+package com.urbanisation_si.microservices_assure.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.urbanisation_si.microservices_assure.modele.Assure;
+
+public interface AssureRepository extends CrudRepository<Assure, Integer> {
+
+}
